9fd895024b8ccd0a55825cc298fec479195a99d9
András Leitereg <leanil94@gmail.com>
2019-10-29 10:26:39 +0100

    Clean up array_sum.

--- app/array_sum/array_sum.cpp
+++ app/array_sum/array_sum.cpp
@@ -2,10 +2,11 @@
 #include "sizes.h"
 #include "util.h"
 #include <benchmark/benchmark.h>
+#include <x86intrin.h>
 
 using namespace std;
 
-double bench0(double *A, int S)
+double array_sum_1var(double *A, int S)
 {
     double sum = 0;
     for (int i = 0; i < S; ++i)
@@ -13,7 +14,7 @@ double bench0(double *A, int S)
     return sum;
 }
 
-double bench1(double *A, int S)
+double array_sum_2var(double *A, int S)
 {
     double sum0 = 0, sum1 = 0;
     for (int i = 0; i < S; i += 2)
@@ -24,7 +25,7 @@ double bench1(double *A, int S)
     return sum0 + sum1;
 }
 
-double bench2(double *A, int S)
+double array_sum_3var(double *A, int S)
 {
     double sum0 = 0, sum1 = 0, sum2 = 0;
     for (int i = 0; i < S; i += 3)
@@ -36,7 +37,7 @@ double bench2(double *A, int S)
     return sum0 + sum1 + sum2;
 }
 
-double bench3(double *A, int S)
+double array_sum_4var(double *A, int S)
 {
     double sum0 = 0, sum1 = 0, sum2 = 0, sum3 = 0;
     for (int i = 0; i < S; i += 4)
@@ -49,7 +50,7 @@ double bench3(double *A, int S)
     return sum0 + sum1 + sum2 + sum3;
 }
 
-double bench4(double *A, int S)
+double array_sum_6var(double *A, int S)
 {
     double sum0 = 0, sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0;
     for (int i = 0; i < S; i += 6)
@@ -64,7 +65,7 @@ double bench4(double *A, int S)
     return sum0 + sum1 + sum2 + sum3 + sum4 + sum5;
 }
 
-double bench5(double *A, int S)
+double array_sum_12var(double *A, int S)
 {
     double sum0 = 0, sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0, sum6 = 0, sum7 = 0, sum8 = 0, sum9 = 0, sum10 = 0, sum11 = 0;
     for (int i = 0; i < S; i += 12)
@@ -85,6 +86,47 @@ double bench5(double *A, int S)
     return sum0 + sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7 + sum8 + sum9 + sum10 + sum11;
 }
 
+double array_sum_1vec(double *A, int S)
+{
+    __m128d sum = _mm_setzero_pd();
+    for (int i = 0; i < S; i += 2)
+    {
+        sum = _mm_add_pd(sum, _mm_load_pd(A + i));
+    }
+    __m128d tmp = _mm_unpackhi_pd(sum, _mm_setzero_pd());
+    sum = _mm_add_pd(sum, tmp);
+    return _mm_cvtsd_f64(sum);
+}
+
+double array_sum_6vec(double *A, int S)
+{
+    __m128d sum0, sum1, sum2, sum3, sum4, sum5;
+    sum0 = sum1 = sum2 = sum3 = sum4 = sum5 = _mm_setzero_pd();
+    for (int i = 0; i < S; i += 12)
+    {
+        sum0 = _mm_add_pd(sum0, _mm_load_pd(A + i));
+        sum1 = _mm_add_pd(sum1, _mm_load_pd(A + i + 2));
+        sum2 = _mm_add_pd(sum2, _mm_load_pd(A + i + 4));
+        sum3 = _mm_add_pd(sum3, _mm_load_pd(A + i + 6));
+        sum4 = _mm_add_pd(sum4, _mm_load_pd(A + i + 8));
+        sum5 = _mm_add_pd(sum5, _mm_load_pd(A + i + 10));
+    }
+    sum0 = _mm_add_pd(sum0, sum1);
+    sum0 = _mm_add_pd(sum0, sum2);
+    sum0 = _mm_add_pd(sum0, sum3);
+    sum0 = _mm_add_pd(sum0, sum4);
+    sum0 = _mm_add_pd(sum0, sum5);
+    sum1 = _mm_unpackhi_pd(sum0, sum1);
+    sum0 = _mm_add_pd(sum0, sum1);
+    return _mm_cvtsd_f64(sum0);
+}
+
+extern "C"
+{
+    double array_sum_asm_nomov(double *A, int S);
+    double array_sum_asm_mov(double *A, int S);
+}
+
 template <int BATCH>
 void seq_sum_args(benchmark::internal::Benchmark *bench)
 {
@@ -100,32 +142,15 @@ void seq_sum(benchmark::State &state, F f)
     set_proc_speed(state, state.range(0) * sizeof(double));
 }
 
-BENCHMARK_CAPTURE(seq_sum, 1var, bench0)->Apply(seq_sum_args<1>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(seq_sum, 2var, bench1)->Apply(seq_sum_args<2>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(seq_sum, 3var, bench2)->Apply(seq_sum_args<3>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(seq_sum, 4var, bench3)->Apply(seq_sum_args<4>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(seq_sum, 6var, bench4)->Apply(seq_sum_args<6>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(seq_sum, 12var, bench5)->Apply(seq_sum_args<12>)->ComputeStatistics("max", max_stats);
-
-extern "C"
-{
-    double bench6(double *A, int S, int rep);
-    double bench7(double *A, int S, int rep);
-    double bench8(double *A, int S, int rep);
-    double bench9(double *A, int S, int rep);
-}
-
-template <typename F>
-void seq_sum_asm(benchmark::State &state, F f)
-{
-    double *data = Data<double>::get();
-    while (state.KeepRunningBatch(state.max_iterations))
-        f(data, state.range(0), state.max_iterations);
-    set_proc_speed(state, state.range(0) * sizeof(double));
-}
-BENCHMARK_CAPTURE(seq_sum_asm, nomov_combine, bench6)->Apply(seq_sum_args<12>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(seq_sum_asm, mov_combine, bench7)->Apply(seq_sum_args<12>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(seq_sum_asm, nomov_nocombine, bench8)->Apply(seq_sum_args<12>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(seq_sum_asm, mov_nocombine, bench9)->Apply(seq_sum_args<12>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(seq_sum, 1var, array_sum_1var)->Apply(seq_sum_args<1>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(seq_sum, 2var, array_sum_2var)->Apply(seq_sum_args<2>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(seq_sum, 3var, array_sum_3var)->Apply(seq_sum_args<3>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(seq_sum, 4var, array_sum_4var)->Apply(seq_sum_args<4>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(seq_sum, 6var, array_sum_6var)->Apply(seq_sum_args<6>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(seq_sum, 12var, array_sum_12var)->Apply(seq_sum_args<12>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(seq_sum, 1vec, array_sum_1vec)->Apply(seq_sum_args<2>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(seq_sum, 6vec, array_sum_6vec)->Apply(seq_sum_args<12>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(seq_sum, asm_nomov, array_sum_asm_nomov)->Apply(seq_sum_args<12>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(seq_sum, asm_mov, array_sum_asm_mov)->Apply(seq_sum_args<12>)->ComputeStatistics("max", max_stats);
 
 BENCHMARK_MAIN();
--- app/array_sum/bench.s
+++ app/array_sum/bench.s
@@ -1,10 +1,9 @@
-    .globl	bench6
-	.type	bench6,@function
-bench6:
+    .globl	array_sum_asm_nomov
+	.type	array_sum_asm_nomov,@function
+array_sum_asm_nomov:
 	.cfi_startproc
 	shlq	$3, %rsi
 	addq	%rdi, %rsi
-1:	movq	%rdi, %rax
 	xorpd	%xmm0, %xmm0
 	xorpd	%xmm1, %xmm1
 	xorpd	%xmm2, %xmm2
@@ -12,50 +11,32 @@ bench6:
 	xorpd	%xmm4, %xmm4
 	xorpd	%xmm5, %xmm5
 	.p2align	4, 0x90
-2:	addpd	0*16(%rax), %xmm0
-	addpd	1*16(%rax), %xmm1
-	addpd	2*16(%rax), %xmm2
-	addpd	3*16(%rax), %xmm3
-	addpd	4*16(%rax), %xmm4
-	addpd	5*16(%rax), %xmm5
-	addq	$6*16, %rax
-	cmpq	%rsi, %rax
-	jb	2b
-	movapd	%xmm5, %xmm6
-	unpckhpd	%xmm5, %xmm6    # xmm6 = xmm6[1],xmm5[1]
-	addsd	%xmm5, %xmm6
-	movapd	%xmm4, %xmm5
-	unpckhpd	%xmm4, %xmm5    # xmm5 = xmm5[1],xmm4[1]
-	addsd	%xmm6, %xmm5
-	addsd	%xmm4, %xmm5
-	movapd	%xmm3, %xmm4
-	unpckhpd	%xmm3, %xmm4    # xmm4 = xmm4[1],xmm3[1]
-	addsd	%xmm5, %xmm4
-	addsd	%xmm3, %xmm4
-	movapd	%xmm2, %xmm3
-	unpckhpd	%xmm2, %xmm3    # xmm3 = xmm3[1],xmm2[1]
-	addsd	%xmm4, %xmm3
-	addsd	%xmm2, %xmm3
-	movapd	%xmm0, %xmm2
-	unpckhpd	%xmm0, %xmm2    # xmm2 = xmm2[1],xmm0[1]
-	addsd	%xmm3, %xmm2
-	addsd	%xmm0, %xmm2
-	movapd	%xmm1, %xmm0
-	unpckhpd	%xmm1, %xmm0    # xmm0 = xmm0[1],xmm1[1]
-	addsd	%xmm2, %xmm0
-	addsd	%xmm1, %xmm0
-	dec    %rdx
-    jnz    1b
+1:	addpd	0*16(%rdi), %xmm0
+	addpd	1*16(%rdi), %xmm1
+	addpd	2*16(%rdi), %xmm2
+	addpd	3*16(%rdi), %xmm3
+	addpd	4*16(%rdi), %xmm4
+	addpd	5*16(%rdi), %xmm5
+	addq	$6*16, %rdi
+	cmpq	%rsi, %rdi
+	jb	1b
+	addpd	%xmm1, %xmm0
+	addpd	%xmm2, %xmm0
+	addpd	%xmm3, %xmm0
+	addpd	%xmm4, %xmm0
+	addpd	%xmm5, %xmm0
+	movapd	%xmm0, %xmm1
+	unpckhpd	%xmm0, %xmm1
+	addpd	%xmm1, %xmm0
 	ret
 	.cfi_endproc
 
-    .globl	bench7
-	.type	bench7,@function
-bench7:
+    .globl	array_sum_asm_mov
+	.type	array_sum_asm_mov,@function
+array_sum_asm_mov:
 	.cfi_startproc
 	shlq	$3, %rsi
 	addq	%rdi, %rsi
-1:	movq	%rdi, %rax
 	xorpd	%xmm0, %xmm0
 	xorpd	%xmm1, %xmm1
 	xorpd	%xmm2, %xmm2
@@ -63,110 +44,30 @@ bench7:
 	xorpd	%xmm4, %xmm4
 	xorpd	%xmm5, %xmm5
 	.p2align	4, 0x90
-2:
-	movapd	0*16(%rax), %xmm6
+1:
+	movapd	0*16(%rdi), %xmm6
 	addpd	%xmm6, %xmm0
-	movapd	1*16(%rax), %xmm7
+	movapd	1*16(%rdi), %xmm7
 	addpd	%xmm7, %xmm1
-	movapd	2*16(%rax), %xmm8
+	movapd	2*16(%rdi), %xmm8
 	addpd	%xmm8, %xmm2
-	movapd	3*16(%rax), %xmm9
+	movapd	3*16(%rdi), %xmm9
 	addpd	%xmm9, %xmm3
-	movapd	4*16(%rax), %xmm10
+	movapd	4*16(%rdi), %xmm10
 	addpd	%xmm10, %xmm4
-	movapd	5*16(%rax), %xmm11
+	movapd	5*16(%rdi), %xmm11
 	addpd	%xmm11, %xmm5
-	addq	$6*16, %rax
-	cmpq	%rsi, %rax
-	jb	2b
-
-	movapd	%xmm5, %xmm6
-	unpckhpd	%xmm5, %xmm6    # xmm6 = xmm6[1],xmm5[1]
-	addsd	%xmm5, %xmm6
-	movapd	%xmm4, %xmm5
-	unpckhpd	%xmm4, %xmm5    # xmm5 = xmm5[1],xmm4[1]
-	addsd	%xmm6, %xmm5
-	addsd	%xmm4, %xmm5
-	movapd	%xmm3, %xmm4
-	unpckhpd	%xmm3, %xmm4    # xmm4 = xmm4[1],xmm3[1]
-	addsd	%xmm5, %xmm4
-	addsd	%xmm3, %xmm4
-	movapd	%xmm2, %xmm3
-	unpckhpd	%xmm2, %xmm3    # xmm3 = xmm3[1],xmm2[1]
-	addsd	%xmm4, %xmm3
-	addsd	%xmm2, %xmm3
-	movapd	%xmm0, %xmm2
-	unpckhpd	%xmm0, %xmm2    # xmm2 = xmm2[1],xmm0[1]
-	addsd	%xmm3, %xmm2
-	addsd	%xmm0, %xmm2
-	movapd	%xmm1, %xmm0
-	unpckhpd	%xmm1, %xmm0    # xmm0 = xmm0[1],xmm1[1]
-	addsd	%xmm2, %xmm0
-	addsd	%xmm1, %xmm0
-	dec    %rdx
-    jnz    1b
+	addq	$6*16, %rdi
+	cmpq	%rsi, %rdi
+	jb	1b
+	addpd	%xmm1, %xmm0
+	addpd	%xmm2, %xmm0
+	addpd	%xmm3, %xmm0
+	addpd	%xmm4, %xmm0
+	addpd	%xmm5, %xmm0
+	movapd	%xmm0, %xmm1
+	unpckhpd	%xmm0, %xmm1
+	addpd	%xmm1, %xmm0
 	ret
 	.cfi_endproc
 
-	.globl	bench8
-	.type	bench8,@function
-bench8:
-	.cfi_startproc
-	shlq	$3, %rsi
-	addq	%rdi, %rsi
-1:	movq	%rdi, %rax
-	xorpd	%xmm0, %xmm0
-	xorpd	%xmm1, %xmm1
-	xorpd	%xmm2, %xmm2
-	xorpd	%xmm3, %xmm3
-	xorpd	%xmm4, %xmm4
-	xorpd	%xmm5, %xmm5
-	.p2align	4, 0x90
-2:	addpd	0*16(%rax), %xmm0
-	addpd	1*16(%rax), %xmm1
-	addpd	2*16(%rax), %xmm2
-	addpd	3*16(%rax), %xmm3
-	addpd	4*16(%rax), %xmm4
-	addpd	5*16(%rax), %xmm5
-	addq	$6*16, %rax
-	cmpq	%rsi, %rax
-	jb	2b
-	dec    %rdx
-    jnz    1b
-	ret
-	.cfi_endproc
-
-	.globl	bench9
-	.type	bench9,@function
-bench9:
-	.cfi_startproc
-	shlq	$3, %rsi
-	addq	%rdi, %rsi
-1:	movq	%rdi, %rax
-	xorpd	%xmm0, %xmm0
-	xorpd	%xmm1, %xmm1
-	xorpd	%xmm2, %xmm2
-	xorpd	%xmm3, %xmm3
-	xorpd	%xmm4, %xmm4
-	xorpd	%xmm5, %xmm5
-	.p2align	4, 0x90
-2:	movapd	0*16(%rax), %xmm6
-	addpd	%xmm6, %xmm0
-	movapd	1*16(%rax), %xmm7
-	addpd	%xmm7, %xmm1
-	movapd	2*16(%rax), %xmm8
-	addpd	%xmm8, %xmm2
-	movapd	3*16(%rax), %xmm9
-	addpd	%xmm9, %xmm3
-	movapd	4*16(%rax), %xmm10
-	addpd	%xmm10, %xmm4
-	movapd	5*16(%rax), %xmm11
-	addpd	%xmm11, %xmm5
-	addq	$6*16, %rax
-	cmpq	%rsi, %rax
-	jb	2b
-	dec    %rdx
-    jnz    1b
-	ret
-	.cfi_endproc
-	
