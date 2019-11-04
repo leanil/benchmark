a64e49a96e1ce3ffc283dd3189293321ab557a90
András Leitereg <leanil94@gmail.com>
2019-11-04 09:30:05 +0100

    Add repetitions and remove redundant benchmarks from dot_prod and mat_sum.

--- app/dot_prod/dot_prod.cpp
+++ app/dot_prod/dot_prod.cpp
@@ -2,87 +2,102 @@
 #include "sizes.h"
 #include "util.h"
 #include <benchmark/benchmark.h>
+#include <x86intrin.h>
 
 using namespace std;
 
-double bench0(double *A, double *B, int S)
+double dot_prod_1var(double *A, double *B, int S, int rep)
 {
-    double sum = 0;
-    for (int i = 0; i < S; ++i)
-        sum += A[i] * B[i];
-    return sum;
+    volatile double ret = 0;
+    while (rep--)
+    {
+        double sum = 0;
+        for (int i = 0; i < S; ++i)
+            sum += A[i] * B[i];
+        ret = sum;
+    }
+    return ret;
 }
 
-double bench1(double *A, double *B, int S)
+double dot_prod_12var(double *A, double *B, int S, int rep)
 {
-    double sum0 = 0, sum1 = 0;
-    for (int i = 0; i < S; i += 2)
+    volatile double ret = 0;
+    while (rep--)
     {
-        sum0 += A[i] * B[i];
-        sum1 += A[i + 1] * B[i + 1];
+        double sum0 = 0, sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0, sum6 = 0, sum7 = 0, sum8 = 0, sum9 = 0, sum10 = 0, sum11 = 0;
+        for (int i = 0; i < S; i += 12)
+        {
+            sum0 += A[i] * B[i];
+            sum1 += A[i + 1] * B[i + 1];
+            sum2 += A[i + 2] * B[i + 2];
+            sum3 += A[i + 3] * B[i + 3];
+            sum4 += A[i + 4] * B[i + 4];
+            sum5 += A[i + 5] * B[i + 5];
+            sum6 += A[i + 6] * B[i + 6];
+            sum7 += A[i + 7] * B[i + 7];
+            sum8 += A[i + 8] * B[i + 8];
+            sum9 += A[i + 9] * B[i + 9];
+            sum10 += A[i + 10] * B[i + 10];
+            sum11 += A[i + 11] * B[i + 11];
+        }
+        ret = sum0 + sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7 + sum8 + sum9 + sum10 + sum11;
     }
-    return sum0 + sum1;
+    return ret;
 }
 
-double bench2(double *A, double *B, int S)
+double dot_prod_3vec(double *A, double *B, int S, int rep)
 {
-    double sum0 = 0, sum1 = 0, sum2 = 0;
-    for (int i = 0; i < S; i += 3)
+    volatile double ret = 0;
+    while (rep--)
     {
-        sum0 += A[i] * B[i];
-        sum1 += A[i + 1] * B[i + 1];
-        sum2 += A[i + 2] * B[i + 2];
+        __m128d sum0, sum1, sum2;
+        sum0 = sum1 = sum2 = _mm_setzero_pd();
+        for (int i = 0; i < S; i += 6)
+        {
+            sum0 = _mm_add_pd(sum0, _mm_mul_pd(_mm_load_pd(A + i), _mm_load_pd(B + i)));
+            sum1 = _mm_add_pd(sum1, _mm_mul_pd(_mm_load_pd(A + i + 2), _mm_load_pd(B + i + 2)));
+            sum2 = _mm_add_pd(sum2, _mm_mul_pd(_mm_load_pd(A + i + 4), _mm_load_pd(B + i + 4)));
+        }
+        sum0 = _mm_add_pd(sum0, sum1);
+        sum0 = _mm_add_pd(sum0, sum2);
+        sum1 = _mm_unpackhi_pd(sum0, sum1);
+        sum0 = _mm_add_pd(sum0, sum1);
+        ret = _mm_cvtsd_f64(sum0);
     }
-    return sum0 + sum1 + sum2;
+    return ret;
 }
 
-double bench3(double *A, double *B, int S)
+double dot_prod_6vec(double *A, double *B, int S, int rep)
 {
-    double sum0 = 0, sum1 = 0, sum2 = 0, sum3 = 0;
-    for (int i = 0; i < S; i += 4)
+    volatile double ret = 0;
+    while (rep--)
     {
-        sum0 += A[i] * B[i];
-        sum1 += A[i + 1] * B[i + 1];
-        sum2 += A[i + 2] * B[i + 2];
-        sum3 += A[i + 3] * B[i + 3];
+        __m128d sum0, sum1, sum2, sum3, sum4, sum5;
+        sum0 = sum1 = sum2 = sum3 = sum4 = sum5 = _mm_setzero_pd();
+        for (int i = 0; i < S; i += 12)
+        {
+            sum0 = _mm_add_pd(sum0, _mm_mul_pd(_mm_load_pd(A + i), _mm_load_pd(B + i)));
+            sum1 = _mm_add_pd(sum1, _mm_mul_pd(_mm_load_pd(A + i + 2), _mm_load_pd(B + i + 2)));
+            sum2 = _mm_add_pd(sum2, _mm_mul_pd(_mm_load_pd(A + i + 4), _mm_load_pd(B + i + 4)));
+            sum3 = _mm_add_pd(sum3, _mm_mul_pd(_mm_load_pd(A + i + 6), _mm_load_pd(B + i + 6)));
+            sum4 = _mm_add_pd(sum4, _mm_mul_pd(_mm_load_pd(A + i + 8), _mm_load_pd(B + i + 8)));
+            sum5 = _mm_add_pd(sum5, _mm_mul_pd(_mm_load_pd(A + i + 10), _mm_load_pd(B + i + 10)));
+        }
+        sum0 = _mm_add_pd(sum0, sum1);
+        sum0 = _mm_add_pd(sum0, sum2);
+        sum0 = _mm_add_pd(sum0, sum3);
+        sum0 = _mm_add_pd(sum0, sum4);
+        sum0 = _mm_add_pd(sum0, sum5);
+        sum1 = _mm_unpackhi_pd(sum0, sum1);
+        sum0 = _mm_add_pd(sum0, sum1);
+        ret = _mm_cvtsd_f64(sum0);
     }
-    return sum0 + sum1 + sum2 + sum3;
+    return ret;
 }
 
-double bench4(double *A, double *B, int S)
+extern "C"
 {
-    double sum0 = 0, sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0;
-    for (int i = 0; i < S; i += 6)
-    {
-        sum0 += A[i] * B[i];
-        sum1 += A[i + 1] * B[i + 1];
-        sum2 += A[i + 2] * B[i + 2];
-        sum3 += A[i + 3] * B[i + 3];
-        sum4 += A[i + 4] * B[i + 4];
-        sum5 += A[i + 5] * B[i + 5];
-    }
-    return sum0 + sum1 + sum2 + sum3 + sum4 + sum5;
-}
-
-double bench5(double *A, double *B, int S)
-{
-    double sum0 = 0, sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0, sum6 = 0, sum7 = 0, sum8 = 0, sum9 = 0, sum10 = 0, sum11 = 0;
-    for (int i = 0; i < S; i += 12)
-    {
-        sum0 += A[i] * B[i];
-        sum1 += A[i + 1] * B[i + 1];
-        sum2 += A[i + 2] * B[i + 2];
-        sum3 += A[i + 3] * B[i + 3];
-        sum4 += A[i + 4] * B[i + 4];
-        sum5 += A[i + 5] * B[i + 5];
-        sum6 += A[i + 6] * B[i + 6];
-        sum7 += A[i + 7] * B[i + 7];
-        sum8 += A[i + 8] * B[i + 8];
-        sum9 += A[i + 9] * B[i + 9];
-        sum10 += A[i + 10] * B[i + 10];
-        sum11 += A[i + 11] * B[i + 11];
-    }
-    return sum0 + sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7 + sum8 + sum9 + sum10 + sum11;
+    double bench6(double *A, double *B, int S, int rep);
 }
 
 template <int BATCH>
@@ -97,32 +112,15 @@ template <typename F>
 void dot_prod(benchmark::State &state, F f)
 {
     double *A = Data<double>::get(0), *B = Data<double>::get(1);
-    for (auto _ : state)
-        benchmark::DoNotOptimize(f(A, B, state.range(0)));
-    set_proc_speed(state, 2 * state.range(0) * sizeof(double));
-}
-
-BENCHMARK_CAPTURE(dot_prod, 1var, bench0)->Apply(dot_prod_args<1>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(dot_prod, 2var, bench1)->Apply(dot_prod_args<2>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(dot_prod, 3var, bench2)->Apply(dot_prod_args<3>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(dot_prod, 4var, bench3)->Apply(dot_prod_args<4>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(dot_prod, 6var, bench4)->Apply(dot_prod_args<6>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(dot_prod, 12var, bench5)->Apply(dot_prod_args<12>)->ComputeStatistics("max", max_stats);
-
-extern "C"
-{
-    double bench6(double *A, double *B, int S, int rep);
-}
-
-template <typename F>
-void dot_prod_asm(benchmark::State &state, F f)
-{
-    double *A = Data<double>::get(0), *B = Data<double>::get(1);
     while (state.KeepRunningBatch(state.max_iterations))
-        f(A, B, state.range(0), state.max_iterations);
+        benchmark::DoNotOptimize(f(A, B, state.range(0), state.max_iterations));
     set_proc_speed(state, 2 * state.range(0) * sizeof(double));
 }
 
-BENCHMARK_CAPTURE(dot_prod_asm, combine, bench6)->Apply(dot_prod_args<12>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(dot_prod, 1var, dot_prod_1var)->Apply(dot_prod_args<1>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(dot_prod, 12var, dot_prod_12var)->Apply(dot_prod_args<12>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(dot_prod, 3vec, dot_prod_3vec)->Apply(dot_prod_args<6>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(dot_prod, 6vec, dot_prod_6vec)->Apply(dot_prod_args<12>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(dot_prod, asm, bench6)->Apply(dot_prod_args<12>)->ComputeStatistics("max", max_stats);
 
 BENCHMARK_MAIN();
--- app/mat_sum/mat_sum.cpp
+++ app/mat_sum/mat_sum.cpp
@@ -6,78 +6,73 @@
 
 using namespace std;
 
-double mat_sum_0(double *a, int n, int m)
+double mat_sum_1var(double *a, int n, int m, int rep)
 {
-    double sum = 0;
-    for (int j = 0; j < m; ++j)
-        for (int i = 0; i < n; ++i)
-            sum += a[i * m + j];
-    return sum;
+    volatile double ret = 0;
+    while (rep--)
+    {
+        double sum = 0;
+        for (int j = 0; j < m; ++j)
+            for (int i = 0; i < n; ++i)
+                sum += a[i * m + j];
+        ret = sum;
+    }
+    return ret;
 }
 
-double mat_sum_1(double *a, int n, int m)
+double mat_sum_6var(double *a, int n, int m, int rep)
 {
-    double sum0 = 0, sum1 = 0;
-    for (int j = 0; j < m; ++j)
-        for (int i = 0; i < n; i += 2)
-        {
-            sum0 += a[i * m + j];
-            sum1 += a[(i + 1) * m + j];
-        }
-    return sum0 + sum1;
+    volatile double ret = 0;
+    while (rep--)
+    {
+        double sum0 = 0, sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0;
+        for (int j = 0; j < m; ++j)
+            for (int i = 0; i < n; i += 6)
+            {
+                sum0 += a[i * m + j];
+                sum1 += a[(i + 1) * m + j];
+                sum2 += a[(i + 2) * m + j];
+                sum3 += a[(i + 3) * m + j];
+                sum4 += a[(i + 4) * m + j];
+                sum5 += a[(i + 5) * m + j];
+            }
+        ret = sum0 + sum1 + sum2 + sum3 + sum4 + sum5;
+    }
+    return ret;
 }
 
-double mat_sum_2(double *a, int n, int m)
+double mat_sum_6vec(double *A, int n, int m, int rep)
 {
-    double sum0 = 0, sum1 = 0, sum2 = 0;
-    for (int j = 0; j < m; ++j)
-        for (int i = 0; i < n; i += 3)
-        {
-            sum0 += a[i * m + j];
-            sum1 += a[(i + 1) * m + j];
-            sum2 += a[(i + 2) * m + j];
-        }
-    return sum0 + sum1 + sum2;
+    volatile double ret = 0;
+    while (rep--)
+    {
+        __m128d sum0, sum1, sum2, sum3, sum4, sum5;
+        sum0 = sum1 = sum2 = sum3 = sum4 = sum5 = _mm_setzero_pd();
+        for (int j = 0; j < m; j += 2)
+            for (int i = 0; i < n; i += 6)
+            {
+                sum0 = _mm_add_pd(sum0, _mm_load_pd(A + i * m + j));
+                sum1 = _mm_add_pd(sum1, _mm_load_pd(A + (i + 1) * m + j));
+                sum2 = _mm_add_pd(sum2, _mm_load_pd(A + (i + 2) * m + j));
+                sum3 = _mm_add_pd(sum3, _mm_load_pd(A + (i + 3) * m + j));
+                sum4 = _mm_add_pd(sum4, _mm_load_pd(A + (i + 4) * m + j));
+                sum5 = _mm_add_pd(sum5, _mm_load_pd(A + (i + 5) * m + j));
+            }
+        sum0 = _mm_add_pd(sum0, sum1);
+        sum0 = _mm_add_pd(sum0, sum2);
+        sum0 = _mm_add_pd(sum0, sum3);
+        sum0 = _mm_add_pd(sum0, sum4);
+        sum0 = _mm_add_pd(sum0, sum5);
+        sum1 = _mm_unpackhi_pd(sum0, sum1);
+        sum0 = _mm_add_pd(sum0, sum1);
+        ret = _mm_cvtsd_f64(sum0);
+    }
+    return ret;
 }
 
-double mat_sum_3(double *a, int n, int m)
+extern "C"
 {
-    double sum0 = 0, sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0;
-    for (int j = 0; j < m; ++j)
-        for (int i = 0; i < n; i += 6)
-        {
-            sum0 += a[i * m + j];
-            sum1 += a[(i + 1) * m + j];
-            sum2 += a[(i + 2) * m + j];
-            sum3 += a[(i + 3) * m + j];
-            sum4 += a[(i + 4) * m + j];
-            sum5 += a[(i + 5) * m + j];
-        }
-    return sum0 + sum1 + sum2 + sum3 + sum4 + sum5;
-}
-
-double mat_sum_6(double *A, int n, int m)
-{
-    __m128d sum0, sum1, sum2, sum3, sum4, sum5;
-    sum0 = sum1 = sum2 = sum3 = sum4 = sum5 = _mm_setzero_pd();
-    for (int j = 0; j < m; j += 2)
-        for (int i = 0; i < n; i += 6)
-        {
-            sum0 = _mm_add_pd(sum0, _mm_load_pd(A + i * m + j));
-            sum1 = _mm_add_pd(sum1, _mm_load_pd(A + (i + 1) * m + j));
-            sum2 = _mm_add_pd(sum2, _mm_load_pd(A + (i + 2) * m + j));
-            sum3 = _mm_add_pd(sum3, _mm_load_pd(A + (i + 3) * m + j));
-            sum4 = _mm_add_pd(sum4, _mm_load_pd(A + (i + 4) * m + j));
-            sum5 = _mm_add_pd(sum5, _mm_load_pd(A + (i + 5) * m + j));
-        }
-    sum0 = _mm_add_pd(sum0, sum1);
-    sum0 = _mm_add_pd(sum0, sum2);
-    sum0 = _mm_add_pd(sum0, sum3);
-    sum0 = _mm_add_pd(sum0, sum4);
-    sum0 = _mm_add_pd(sum0, sum5);
-    sum1 = _mm_unpackhi_pd(sum0, sum1);
-    sum0 = _mm_add_pd(sum0, sum1);
-    return _mm_cvtsd_f64(sum0);
+    double mat_sum_5(double *a, int n, int m, int rep);
 }
 
 template <int BATCH_0, int BATCH_1>
@@ -90,30 +85,14 @@ template <typename F>
 void stride_sum(benchmark::State &state, F f)
 {
     double *data = Data<double>::get();
-    for (auto _ : state)
-        benchmark::DoNotOptimize(f(data, state.range(0), state.range(1)));
-    set_proc_speed(state, state.range(0) * state.range(1) * sizeof(double));
-}
-
-BENCHMARK_CAPTURE(stride_sum, 1var, mat_sum_0)->Apply(stride_sum_args<1, 1>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(stride_sum, 2var, mat_sum_1)->Apply(stride_sum_args<2, 1>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(stride_sum, 3var, mat_sum_2)->Apply(stride_sum_args<3, 1>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(stride_sum, 6var, mat_sum_3)->Apply(stride_sum_args<6, 1>)->ComputeStatistics("max", max_stats);
-BENCHMARK_CAPTURE(stride_sum, 6vec, mat_sum_6)->Apply(stride_sum_args<6, 2>)->ComputeStatistics("max", max_stats);
-
-extern "C"
-{
-    double mat_sum_5(double *a, int n, int m, int rep);
-}
-
-template <typename F>
-void stride_sum_asm(benchmark::State &state, F f)
-{
-    double *A = Data<double>::get();
     while (state.KeepRunningBatch(state.max_iterations))
-        f(A, state.range(0), state.range(1), state.max_iterations);
+        benchmark::DoNotOptimize(f(data, state.range(0), state.range(1), state.max_iterations));
     set_proc_speed(state, state.range(0) * state.range(1) * sizeof(double));
 }
-BENCHMARK_CAPTURE(stride_sum_asm, 6var, mat_sum_5)->Apply(stride_sum_args<6, 2>)->ComputeStatistics("max", max_stats);
+
+BENCHMARK_CAPTURE(stride_sum, 1var, mat_sum_1var)->Apply(stride_sum_args<1, 1>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(stride_sum, 6var, mat_sum_6var)->Apply(stride_sum_args<6, 1>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(stride_sum, 6vec, mat_sum_6vec)->Apply(stride_sum_args<6, 2>)->ComputeStatistics("max", max_stats);
+BENCHMARK_CAPTURE(stride_sum, asm, mat_sum_5)->Apply(stride_sum_args<6, 2>)->ComputeStatistics("max", max_stats);
 
 BENCHMARK_MAIN();
